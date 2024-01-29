a44e1af55112e67bd33bbb1abc8ac4530a0fd253
adamwise <awise.developer@gmail.com>
2024-01-28 23:42:31 -0800

    seperaed the Form copmonent, and its children components, AccordianItem and Prefernces

---
+++ src/AccordianItem.js
@@ -0,0 +1,31 @@
+import Prefrences from "./Prefrences";
+import React, { useState } from "react";
+
+function AccordionItem({ formData, handleFormInput, onButtonClick }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number"></p>
+      <p className="number">Select classes and preferences</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+
+      {isOpen && (
+        <div className="content-box">
+          <Prefrences
+            formData={formData}
+            handleFormInput={handleFormInput}
+            onButtonClick={onButtonClick}
+            handleToggle={handleToggle}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AccordionItem;
---
+++ src/Form.js
@@ -0,0 +1,22 @@
+import AccordionItem from "./AccordianItem";
+
+function Form({ formData, handleFormInput, onButtonClick }) {
+  //<All4Quarters schedule={schedule} />
+  return (
+    <div className="accordion">
+      <h3 style={{ color: "red" }}>
+        Returning students: Please check all classes you have taken so far.
+      </h3>
+      <h3 style={{ color: "green" }}>
+        First time students: Please fill in your preferences.
+      </h3>
+      <AccordionItem
+        formData={formData}
+        handleFormInput={handleFormInput}
+        onButtonClick={onButtonClick}
+      />
+    </div>
+  );
+}
+
+export default Form;
